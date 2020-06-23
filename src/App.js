df06004d2173af1f2ad19ae6b2445c6f0052de83
howan <oke2103@gmail.com>
2020-06-23 12:33:59 +0900

    Apply Material-ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React,{ Component }  from 'react';
 import './App.css';
 // import { render } from '@testing-library/react';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root : {
+    width : '100%',
+    marginTop : theme.spacing.unit*3,
+    overflowX : "auto"
+  },
+  table : {
+    minWidth : 1080
+  }
+})
 
 const customers = [{
   'id'        : 1,
@@ -13,14 +31,28 @@ const customers = [{
 
 class App extends Component {
   render() {
+    debugger;
+    const { classes } = this.props;
     return (
-      customers.map(c => {
-        return (
-          <Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
-        );
-      })
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>직업</TableCell>
+            <TableCell>직업</TableCell>
+            <TableCell>직업</TableCell>
+            <TableCell>직업</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            { customers.map(c => { return ( <Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,37 +1,17 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 // React에 컴포넌트 라이브러리에는 render라는 함수가 포함되어 있다.
 class Customer extends React.Component{
     render(){
         return(
-            <div>
-                <CustomerProfile id={this.props.id} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gendder} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell>{this.props.ticker}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
-
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
-
 export default Customer;
