b1db56f6b8953e021905964c699d85ec1302d2e3
howan <oke2103@gmail.com>
2020-07-01 12:50:26 +0900

    Add Node.js Express Server Module

---
+++ server.js
@@ -0,0 +1,14 @@
+// require는 Node.Js의 모듈 로딩 시스템
+const express = require('express');
+const bodyParser = require('body-parser');
+const app  = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
+
+app.get('/api/hello',(req, res) => {
+    res.send({message : 'Hello Express!'});
+});
+
+app.listen(port, () => console.log(`Listing on port ${port}`));
